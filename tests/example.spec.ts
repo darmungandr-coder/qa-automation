86c3c144a51d7651c6effa64c2d653437ebfe3e4
darmungandr-coder <darmungandr@gmail.com>
2026-03-06 00:43:41 +0100

    First simple login test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test.beforeEach('Base login test', async ({page}) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+})
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+test('Login test', async ({page}) => {
+  const username = page.getByRole('textbox', {name: 'Username'})
+  const password = page.getByRole('textbox', {name: 'Password'})
+  await username.click()
+  await username.fill('Admin')
+
+  await password.click()
+  await password.fill('admin123')
+
+  await page.getByRole('button', {name: 'Login'}).click()
+
+})
